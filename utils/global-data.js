313f7306b0861471189f181de64481738eaacadd
Muhammad <muhammad.1987@gmail.com>
2022-09-05 17:53:43 +0800

    Updated theme details

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Muhammad';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Muhammad.my Blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'All%20rights%20reserved.%20Made%20in%20%F0%9F%87%B2%F0%9F%87%BE.';
 
   return {
     name,
