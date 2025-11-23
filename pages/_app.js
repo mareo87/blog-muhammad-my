5e9f209ab0f54c6b55a4df0d7ec233c204e1e190
Muhammad <muhammad.1987@gmail.com>
2025-11-23 16:05:08 +0800

    feat: add Google Analytics integration (GA placeholder)

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <span className="theme-antigravity" />
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
